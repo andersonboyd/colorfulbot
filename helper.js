45f433e4e1e047a7791480a6d4f259ba97943362
Anderson <andersonswaimboyd@gmail.com>
2019-11-06 20:23:27 -0600

    still debugging -__-

--- helper.js
+++ helper.js
@@ -1,21 +1,10 @@
 module.exports = {
     randomColor: function(){
         var colorArr = [];
-        for(var i = 0; i < 3, i++;){
+        for(var i = 0; i < 3; i++){
             var color = Math.floor(Math.random() * (255 - 0) + 0);
             colorArr.push(color);
         };
         return colorArr;
-    },
-    getColor: function(app, id){
-        app.get(`https://thecolorapi.com/id?rgb=${id}&format=JSON`, function(err, data){
-            if (err){
-                console.log(err.stack);
-                res.status(500).send();
-            }else{
-                console.log(data);
-                res.status(200).json(data);
-            }
-        })
     }
 };
--- server.js
+++ server.js
@@ -1,26 +1,37 @@
 require("dotenv").config();
 const express = require("express");
 const helper = require("./helper");
+const axios = require("axios");
 const twitter = require("./twitter");
 const app = express();
 
 app.use(express.static("public"));
 
 app.all(`/${process.env.BOT_ENDPOINT}`, function(req, res){
-    var id = helper.randomColor().split(",").toString().join(",");
-    var data = helper.getColor(app, id);
-    var name = data.name.value;
-    var img = data.img.bare;
-    twitter.postColor(name, img, function(err, cb){
-        if(err){
-            console.log(err);
-            cb(err);
-            res.status(500).send();
-        }else{
-            cb(null);
-            res.status(200).send();
-        }
-    })
+    var colorId = helper.randomColor();
+    var id = colorId.toString();
+    axios.get(`http://thecolorapi.com/id?rgb=${id}&format=HTML`)
+    .then(function(response){
+        var search = response.data;
+        console.log(search);
+        var name = search.name.value;
+        var img = search.image.bare;
+        twitter.postColor(name, img, function(err){
+            if(err){
+                console.log(err);
+                res.status(500).send();
+            }else{
+                res.status(200).send();
+            }
+        });
+    }).catch(function(err){
+        console.log(err.stack);
+    });
+    // var name = data.name.value;
+    // var img = data.img.bare;
+    // console.log(name);
+    // console.log(img);
+
 });
 
 var listener = app.listen(process.env.PORT, function(){
