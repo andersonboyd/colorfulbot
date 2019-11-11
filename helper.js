0a205b773b07dfc10c58669d567929e022cd802e
Anderson <andersonswaimboyd@gmail.com>
2019-11-11 14:16:36 -0600

    working around issues with svg

--- helper.js
+++ helper.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+const path = require("path");
+const request = require("request");
+
 module.exports = {
     randomColor: function(){
         var colorArr = [];
@@ -6,5 +10,71 @@ module.exports = {
             colorArr.push(color);
         };
         return colorArr;
+    },
+    loadImageAssets: function(cb){
+        console.log("reading assets folder");
+        var that = this;
+        fs.readFile("./assets", "utf8", function(err, data){
+            if(err){
+                console.log("error: ", err);
+                if(cb){
+                    cb(err);
+                }
+                return false;
+            }
+            data = data.split('\n');
+            var data_json = JSON.parse('[' + data.join(',').slice(0, -1) + ']'),
+                deleted_images = data_json.reduce(function(filtered, data_img){
+                    if(data_img.deleted) {
+                        var value = { name: data_img.name, newProperty: "deleted" }
+                        filtered.push(data_img.uuid);
+                    }
+                    return filtered;
+                }, []),
+                img_urls = [];
+                
+            for (var i = 0, j = data.length; i < j; i++){
+                if (data[i].length){
+                    var img_data = JSON.parse(data[i]),
+                        img_url = img_data.url;
+
+                    if(image_url && deleted_images.indexOf(img_data.uuid) === -1 && that.extensionCheck(image_url)){
+                        var file_name = that.getFilenameFromUrl.split('%2F')[1];
+                        img_urls.push(image_url);
+                    }
+                }
+            }
+            if(cb){
+                cb(null, img_urls);
+            }
+        });
+    },
+    loadRemoteImage: function(img_url, cb){
+        if(!img_url){
+            console.log('missing remote image URL');
+            return false;
+        }
+        console.log(`loading remote image: ${img_url} ...`);
+        request({url: img_url, encoding: null}, function(err, res, body){
+            if(!err && res.statusCode == 200) {
+                var b64content = 'data:' + res.headers['content-type'] + ';base64,';
+                if(cb){
+                    cb(null, body.toString('base64'));
+                }else{
+                    console.log('error; ', err);
+                    if(cb){
+                        cb(err);
+                    }
+                }
+            }
+        });
+    },
+    extensionCheck: function(url) {
+        var file_extension = path.extname(url).toLowerCase(),
+            extensions = ['.png', '.jpg', '.jpeg', '.gif'];
+        return extensions.indexOf(file_extension) !== -1;
+    },
+    getFilenameFromUrl: function(url) {
+        return url.substring(url.lastIndexOf('/') + 1);
     }
 };
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+const fs = require("fs");
 const express = require("express");
 const helper = require("./helper");
 const axios = require("axios");
+const cheerio = require("cheerio");
+const svg2png = require("svg2png");
 const twitter = require("./twitter");
+
 const app = express();
 
 app.use(express.static("public"));
@@ -10,28 +14,52 @@ app.use(express.static("public"));
 app.all(`/${process.env.BOT_ENDPOINT}`, function(req, res){
     var colorId = helper.randomColor();
     var id = colorId.toString();
-    axios.get(`http://thecolorapi.com/id?rgb=${id}&format=HTML`)
+    axios.get(`http://thecolorapi.com/id?rgb=${id}&format=JSON`)
     .then(function(response){
         var search = response.data;
         console.log(search);
         var name = search.name.value;
         var img = search.image.bare;
-        twitter.postColor(name, img, function(err){
-            if(err){
-                console.log(err);
-                res.status(500).send();
-            }else{
-                res.status(200).send();
-            }
+        var $ = cheerio.load(img);
+        var svg = $("svg").children();
+        console.log(svg);
+        svg2png(svg, {width: 360, height: 360})
+            .then(function(buffer){
+                fs.writeFile(`/assets/${name}.png`, buffer);
+                helper.loadImageAssets(function(err, asset_urls){
+                    if(err){
+                        console.log(err);
+                        res.sendStatus(500);
+                    }
+                    else{
+                        helper.loadRemoteImage(asset_urls.indexOf(asset_urls.length - 1), function(err, img_data){
+                            if (err) {
+                                console.log(err);
+                                res.sendStatus(500);
+                            }
+                            else{
+                                twitter.postColor(name, img_data, function(err, data){
+                                    if(err){
+                                        console.log(err);
+                                        res.sendStatus(500);
+                                    }else{
+                                        console.log(data);
+                                        res.sendStatus(200);
+                                    }
+                                });
+                            }
+                        })
+                    }
+                })
+            .catch(function(err){
+                console.error(err);
+            });
+        }).catch(function(err){
+            console.error(err);
         });
     }).catch(function(err){
         console.log(err.stack);
     });
-    // var name = data.name.value;
-    // var img = data.img.bare;
-    // console.log(name);
-    // console.log(img);
-
 });
 
 var listener = app.listen(process.env.PORT, function(){
